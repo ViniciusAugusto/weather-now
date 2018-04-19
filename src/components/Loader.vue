ddf85ebd2ff52250405ded68759752150dac2791
vinicius.cunha <vinicius.cunha@virtualage.com.br>
2018-04-19 15:44:48 -0300

    loading

---
+++ src/components/Loader.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="loader">
+    <img :src="loader" alt="Carregando informações...">
+  </section>
+</template>
+
+
+<script>
+export default {
+  props: ['loader']
+}
+</script>
+
+
+<style scoped>
+.loader {
+  width: 100vw;
+  height: 100vh;
+  overflow-x: hidden;
+  background-color: var(--background);
+  box-shadow: 0 0 .5px 0 var(--box-shadow) inset;
+  text-align: center;
+  display: table;
+}
+.loader img {
+  position: absolute;
+  width: 100px;
+  height: 50px;
+  top: 50%;
+  left: 50%;
+  margin-left: -50px;
+  margin-top: -25px;
+}
+</style>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,27 +1,31 @@
 <template>
   <div class="dashboard">
-    <citie :weather="weather"></citie>
+    <loader v-show="load" :loader="loaderImg"></loader>
+    <citie v-show="!load" :weather="weather"></citie>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
 import Citie from '@/components/Citie'
+import Loader from '@/components/Loader'
 
 export default {
   created () {
     this.mountDataWeather()
     window.clearInterval(this.interval)
-    this.interval = window.setInterval(() => this.mountDataWeather(), 600000) // update data in 10minutes
+    this.interval = window.setInterval(() => this.mountDataWeather(), 10000) // 600000 update data in 10minutes
   },
   computed: mapGetters({
     weather: 'weather',
     cities: 'cities'
   }),
-  components: { Citie },
+  components: { Citie, Loader },
   data () {
     return {
-      interval: null
+      interval: null,
+      loaderImg: require('../assets/img/loader.svg'),
+      load: true
     }
   },
   methods: {
@@ -36,6 +40,7 @@ export default {
       Promise
         .all(promisses)
         .then((values) => this.setWeatherCities(values))
+        .then(() => (this.load = false))
     }
   },
   name: 'dashboard'
